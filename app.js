fee084b8eb81bbdf44fac9887671200c32606330
duykhanh <duykhanh1983@gmail.com>
2017-07-24 13:48:16 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
+// app.js
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
 var express = require('express');
+var app = express();
 var http = require('http');
 var https = require('https');
 var server = require('http').createServer(app);
-var app = express();
+var io = require('socket.io')(server);
+
+var bodyParser = require("body-parser");
+
+console.log("Folder:"+__dirname);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(express.static(__dirname + '/bower_components'));
 app.get('/', function (req, res, next) {
     res.sendFile(__dirname + '/index.html');
 });
+var port = 3000;
+server.listen(process.env.PORT || port);
