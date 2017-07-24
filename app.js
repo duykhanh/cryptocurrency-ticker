d0ccea37b48f360a98caa60439c18f2923f1b383
duykhanh <duykhanh1983@gmail.com>
2017-07-24 11:05:38 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 var express = require('express');
+var http = require('http');
+var https = require('https');
+var server = require('http').createServer(app);
 var app = express();
 app.get('/', function (req, res, next) {
     res.sendFile(__dirname + '/index.html');
